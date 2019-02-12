070d5c00fd5d694866fea7f8ca0a0e25b5b0c82f
vyskoutaris <vyskoutaris@hotmail.com>
2019-02-12 20:35:59 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -46,7 +46,7 @@ end
 
 def shortened_tweet_truncator(tweet)
   if tweet.length > 140
-    word_substituter(tweet).truncate(140)
+    [word_substituter(tweet)].truncate(140)
   else
     tweet
   end
