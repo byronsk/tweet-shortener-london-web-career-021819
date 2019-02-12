41aad03f03caf5c0ff6f0e5e4c9453be827fb52d
vyskoutaris <vyskoutaris@hotmail.com>
2019-02-12 20:06:22 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -4,14 +4,14 @@ def dictionary
   
   dictionary = {
     
-    "hello" => 'hi',
-    "to" => '2',
-    "two" => '2',
-    "too" => '2',
-    "for" => '4',
-    "four" => '4',
-    'be' => 'b',
-    'you' => 'u',
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
     "at" => "@",
     "and" => "&"
   }
